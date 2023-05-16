1ea0d302d35c08299157e61efd9c51b14be877e6
Robert <robinho1989@wp.pl>
2023-05-16 21:47:46 +0100

    bpm implementation added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,14 @@
 import { DrumContainer } from '../drumContainer/DrumContainer';
+import { Metronome } from '../metronome/Metronome';
+import { useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
+	const [tempo, setTempo] = useState(120);
+	console.log(tempo)
 	return (
 		<div className={styles.appContainer}>
+			<Metronome tempo={tempo} setTempo={setTempo} />
 			<DrumContainer />
 		</div>
 	);
